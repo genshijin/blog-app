a7c09caca0a3f87018d126acbd5d0ce53c570f6b
genshijin <vodka041619893616@outlook.jp>
2019-06-26 13:37:27 +0900

    Make controller & view

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  var $body = $('body');
+  var $btn = $('.menu-trigger');
+
+  //開閉用ボタンをクリックでクラスの切替え
+  $('#js__btn').on('click', function () {
+    $body.toggleClass('open');
+    $btn.toggleClass('active');
+  });
+
+  //メニュー名以外の部分をクリックで閉じる
+  $('#js__nav').on('click', function () {
+    $body.removeClass('open');
+    $btn.removeClass('active');
+  });
+});
