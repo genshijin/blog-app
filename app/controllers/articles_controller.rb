dee68b2ad61e48d545d5f6e081e3857bc6d346d9
genshijin <vodka041619893616@outlook.jp>
2019-06-27 07:21:47 +0900

    articles controller maked

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,34 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Article.create(article_params)
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    if article.user_id == current_user.id
+      article.destroy
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    if article.user_id == current_user.id
+      article.update(article_params)
+    end
   end
 end
